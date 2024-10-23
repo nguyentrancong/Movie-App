7ae2c21ed32a2280bf4d7507eb74a3a60c960189
Nguyen Tran Cong <catbui2805@gmail.com>
2024-10-23 17:19:49 +0700

    Implement actor list (#1)
    Co-authored-by: Nguyen Tran Cong <nguyentrancong>

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Loading from "../components/Loading";
 import Banner from "../components/MediaDetail/Banner";
+import ActorList from "../components/MediaDetail/ActorList";
 
 const MovieDetail = () => {
   const { id } = useParams();
@@ -42,6 +43,16 @@ const MovieDetail = () => {
     return (
       <div>
         <Banner mediaInfo={movieInfo} />
+        <div className="bg-black text-[1.2vw] text-white">
+          <div className="mx-auto flex max-w-screen-xl gap-6 px-6 py-10">
+            <div className="flex-[2]">
+              <ActorList actors={movieInfo.credits?.cast || []} />
+            </div>
+            <div className="flex-[1]">
+              <p className="mb-4 text-[1.4vw] font-bold">Information</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
