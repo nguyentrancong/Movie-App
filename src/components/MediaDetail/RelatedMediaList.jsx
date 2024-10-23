759da6c5c9e444ec44d6fbcc516d8cc193e43046
Nguyen Tran Cong <nguyentrancong>
2024-10-23 17:47:24 +0700

    Implement UI and Retrived data

--- src/components/MediaDetail/RelatedMediaList.jsx
+++ src/components/MediaDetail/RelatedMediaList.jsx
@@ -1,5 +1,26 @@
-const RelatedMediaList = () => {
-  return <div>RelatedMovieList</div>;
+import MovieCard from "@components/MediaList/MovieCard";
+
+const RelatedMediaList = ({ mediaList = [] }) => {
+  return (
+    <div className="mt-4">
+      <p className="mb-4 text-[1.4vw] font-bold">Related</p>
+      <div>
+        {mediaList.map((media) => {
+          return (
+            <MovieCard
+              key={media.id}
+              id={media.id}
+              title={media.title}
+              releaseDate={media.release_date}
+              poster={media.poster_path}
+              point={media.vote_average}
+              mediaType={media.media_type}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default RelatedMediaList;
