2ffed21ea6385fb59ef419c5a8bc338f1aac9d8d
Nguyen Tran Cong <nguyentrancong>
2024-10-24 09:54:49 +0700

    Implement movie info

---
+++ src/components/MediaDetail/MovieInfo.jsx
@@ -0,0 +1,53 @@
+import { currencyFormatter } from "@libs/utils";
+
+const MovieInfo = ({ mediaInfo = {} }) => {
+  return (
+    <div>
+      <p className="mb-4 text-[1.4vw] font-bold">Information</p>
+      <div className="mb-4">
+        <p className="font-bold">Original Name</p>
+        <p>{mediaInfo?.original_title}</p>
+      </div>
+      <div className="mb-4">
+        <p className="font-bold">Original Country</p>
+        <p>
+          {(mediaInfo?.origin_country || []).map((item) => {
+            return (
+              <img
+                key={item}
+                src={`https://flagcdn.com/48x36/${item.toLowerCase()}.png`}
+                className="mr-1 mt-1 w-[1.4vw]"
+              />
+            );
+          })}
+        </p>
+      </div>
+      <div className="mb-4">
+        <p className="font-bold">Status</p>
+        <p>{mediaInfo?.status}</p>
+      </div>
+      <div className="mb-4">
+        <p className="font-bold">Budget</p>
+        <p>{currencyFormatter(mediaInfo?.budget)}</p>
+      </div>
+      <div className="mb-4">
+        <p className="font-bold">Revenue</p>
+        <p>{currencyFormatter(mediaInfo?.revenue)}</p>
+      </div>
+      <div className="mb-4">
+        <p className="font-bold"></p>
+        <p></p>
+      </div>
+      <div className="mb-4">
+        <p className="font-bold"></p>
+        <p></p>
+      </div>
+      <div className="mb-4">
+        <p className="font-bold"></p>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieInfo;
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -4,6 +4,7 @@ import Loading from "@components/Loading";
 import Banner from "@components/MediaDetail/Banner";
 import ActorList from "@components/MediaDetail/ActorList";
 import RelatedMediaList from "@components/MediaDetail/RelatedMediaList";
+import MovieInfo from "@components/MediaDetail/MovieInfo";
 
 const MovieDetail = () => {
   const { id } = useParams();
@@ -68,13 +69,13 @@ const MovieDetail = () => {
       <div>
         <Banner mediaInfo={movieInfo} />
         <div className="bg-black text-[1.2vw] text-white">
-          <div className="mx-auto flex max-w-screen-xl gap-6 px-6 py-10">
+          <div className="mx-auto flex max-w-screen-xl gap-6 px-6 py-10 sm:gap-8">
             <div className="flex-[2]">
               <ActorList actors={movieInfo.credits?.cast || []} />
               <RelatedMediaList mediaList={relatedMovies} />
             </div>
-            <div className="flex-[1]">
-              <p className="mb-4 text-[1.4vw] font-bold">Information</p>
+            <div className="flex-1">
+              <MovieInfo mediaInfo={movieInfo} />
             </div>
           </div>
         </div>
