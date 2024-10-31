78275362777b50b3bcb19c883b142933a2b49c8f
Nguyen Tran Cong <nguyentrancong>
2024-10-31 15:28:37 +0700

    Demo Movie app

--- src/libs/utils.js
+++
@@ -1,8 +0,0 @@
-export const currencyFormatter = (number, currency = "USD") => {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency,
-  });
-
-  return formatter.format(number);
-};
